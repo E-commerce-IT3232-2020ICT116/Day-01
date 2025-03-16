0eeb751d89e1488ab8ca381395b4aa12cd5cfd15
Chesintha <139671816+Chesintha@users.noreply.github.com>
2025-03-16 16:21:02 +0000

    Q4 done

---
+++ Q4/four.java
@@ -0,0 +1,20 @@
+public class four{
+    public static void main(String args[]){
+        for(int i=1;i<=20;i+=2){
+            System.out.println(i);
+        }
+    }
+}
+
+/*@Chesintha ➜ /workspaces/Day-01/Q4 (main) $ javac four.java
+@Chesintha ➜ /workspaces/Day-01/Q4 (main) $ java four
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19 */
