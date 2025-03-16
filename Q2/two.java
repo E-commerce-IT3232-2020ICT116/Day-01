c9678c5919c4140261c7b49ccb32f516f3ef97de
Chesintha <139671816+Chesintha@users.noreply.github.com>
2025-03-16 16:08:49 +0000

    Q2 done

---
+++ Q2/two.java
@@ -0,0 +1,23 @@
+public class two {
+    public static void main(String args[]){
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
+
+
+/*@Chesintha ➜ /workspaces/Day-01/Q2 (main) $ javac two.java
+@Chesintha ➜ /workspaces/Day-01/Q2 (main) $ java two
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
