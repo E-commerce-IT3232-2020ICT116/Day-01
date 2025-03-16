c089df33228a1a4833bcede95cd085e6fd066da7
Chesintha <139671816+Chesintha@users.noreply.github.com>
2025-03-16 16:15:39 +0000

    Q3 done

---
+++ Q3/three.java
@@ -0,0 +1,20 @@
+public class three{
+    public static void main(String args[]){
+        for(int i=10; i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
+
+/*@Chesintha ➜ /workspaces/Day-01/Q3 (main) $ javac three.java
+@Chesintha ➜ /workspaces/Day-01/Q3 (main) $ java three
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1 */
