1bd3a40aa4cfe0955feb15b8871f8f26ef436f06
Chesintha <139671816+Chesintha@users.noreply.github.com>
2025-03-16 17:21:30 +0000

    Q7 done

---
+++ Q7/Seven.java
@@ -0,0 +1,20 @@
+public class Seven{
+    public static void main(String[] args) {
+
+        Library library = new Library(5);
+
+        library.addBook(new PrintedBook("Enid blyton", "Five on a Treasure Island", 1942, 180));
+        library.addBook(new EBook("George Orwell", "1984", 1949, 1.5, "PDF"));
+        library.addBook(new AudioBook("Michelle Obama", "Becoming", 2018, 19.5, "Michelle Obama"));
+
+        library.displayBooks();
+    }
+}
+
+/*@Chesintha ➜ /workspaces/Day-01/Q7 (main) $ javac Seven.java
+@Chesintha ➜ /workspaces/Day-01/Q7 (main) $ java Seven
+
+Library Collection:
+Printed Book: Enid blyton by Five on a Treasure Island, Year: 1942, Pages: 180
+E-Book: George Orwell by 1984, Year: 1949, File Size: 1.5MB, Format: PDF
+Audio Book: Michelle Obama by Becoming, Year: 2018, Duration: 19.5 hours, Narrated by: Michelle Obama */
